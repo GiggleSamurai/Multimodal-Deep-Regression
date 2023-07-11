554b40313b3c1509f4fe33f4e96bb4a490153add
Louis Wong <gigglesamurai@gmail.com>
2023-07-11 16:06:35 -0700

    update

--- util/utilities.py
+++ util/utilities.py
@@ -1,70 +1,40 @@
-import math
-import time
-import random
-
 import torch
 import torch.optim as optim
 import torch.nn as nn
 import numpy as np
-from tqdm import tqdm_notebook
 
-RANDOM_SEED = 0
-
-def set_seed():
-    torch.manual_seed(RANDOM_SEED)
-    np.random.seed(RANDOM_SEED)
-    random.seed(RANDOM_SEED)
-
-def set_seed_nb():
-    torch.manual_seed(RANDOM_SEED)
-    np.random.seed(RANDOM_SEED + 1)
-
-def deterministic_init(net: nn.Module):
-    for p in net.parameters():
-        if p.data.ndimension() >= 2:
-            set_seed_nb()
-            nn.init.xavier_uniform_(p.data)
-        else:
-            nn.init.zeros_(p.data)
-
-def train(model, dataloader, optimizer, criterion, scheduler=None, device='cpu'):
+# training function
+def train(model, dataloader, criterion, optimizer, device='cpu'):
     model.train()
-    total_loss = 0.
-    progress_bar = tqdm_notebook(dataloader, ascii=True)
+    total_loss = 0.0
+    for inputs, targets in dataloader:
+        inputs, targets = inputs.to(device), targets.to(device)
 
-    for batch_idx, data in enumerate(progress_bar):
-        source = data[0].to(device)
-        target = data[1].to(device)
+        # forward pass
+        outputs = model(inputs)
+        loss = criterion(outputs, targets)
 
+        # backward pass and optimization
         optimizer.zero_grad()
-        prediction = model(source)
-
-        loss = criterion(prediction, target)
         loss.backward()
-        torch.nn.utils.clip_grad_norm_(model.parameters(), 0.5)
         optimizer.step()
 
         total_loss += loss.item()
-        progress_bar.set_description_str(
-            "Batch: %d, Loss: %.4f" % ((batch_idx + 1), loss.item()))
 
-    return total_loss, total_loss / len(dataloader)
+    avg_loss = total_loss / len(dataloader)
+    return total_loss, avg_loss
 
+# evaluation function
 def evaluate(model, dataloader, criterion, device='cpu'):
     model.eval()
-    total_loss = 0.
+    total_loss = 0.0
     with torch.no_grad():
-        progress_bar = tqdm_notebook(dataloader, ascii=True)
-        for batch_idx, data in enumerate(progress_bar):
-            source = data[0].to(device)
-            target = data[1].to(device)
+        for inputs, targets in dataloader:
+            inputs, targets = inputs.to(device), targets.to(device)
 
-            prediction = model(source)
-            loss = criterion(prediction, target)
-
+            outputs = model(inputs)
+            loss = criterion(outputs, targets)
             total_loss += loss.item()
-            progress_bar.set_description_str(
-                "Batch: %d, Loss: %.4f" % ((batch_idx + 1), loss.item()))
 
     avg_loss = total_loss / len(dataloader)
     return total_loss, avg_loss
