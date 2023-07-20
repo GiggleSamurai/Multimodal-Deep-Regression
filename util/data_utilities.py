74eb191f7a2b26f05dfc0f0cb1ddcccc17797bb0
Ahmed <ahmedkasalih@gmail.com>
2023-07-19 20:28:12 -0400

    Update

--- util/data_utilities.py
+++ util/data_utilities.py
@@ -86,7 +86,6 @@ def process_data(input_type, addition_parameters=None, verbose=False, device='cp
             vf = vf/ 255.0
 
         # resize the tensor to 1024x576
-<<<<<<< HEAD
         if resize_tensors:
             vf = resize_tensor(vf)
 
@@ -104,13 +103,13 @@ def process_data(input_type, addition_parameters=None, verbose=False, device='cp
 
             if verbose:
                     print(f'Resize to tensor size: {vf.shape}')
-=======
+
         #vf = resize_tensor(vf)
         if shrink > 1:
             vf = shrink_video(vf,shrink=shrink)
-        if verbose:
-                print(f'Resize to tensor size: {vf.shape}')
->>>>>>> d7f4f7e21cb91b9b4cc69a81757f13fb919f4e70
+            if verbose:
+                    print(f'Resize to tensor size: {vf.shape}')
+
                 
         x_file_path = f"{x_dir}{tiktok_video_id}_x_tensor.pt"
         y_file_path = f"{y_dir}{tiktok_video_id}_y_tensor.pt"
