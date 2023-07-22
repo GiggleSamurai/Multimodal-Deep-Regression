2abb24e4368a09fd27c407a95ce5727638dca719
jasonxubw <jasonxubw@gmail.com>
2023-07-21 22:07:53 -0700

    Add device and os configs to extract_embeddings

--- util/audio_utilities0.py
+++ util/audio_utilities0.py
@@ -9,13 +9,15 @@ from tqdm import tqdm
 # Extracts audio into wav format 
 def extract_audio(video_folder_path: str, output_dir: str):
     print('Extract in audio from video pack to .wav format..')
-    if not os.path.exists(output_dir):
-        os.makedirs(output_dir)
-    for file in tqdm(os.listdir(video_folder_path)):
-        video_path = os.path.join(video_folder_path, file)
+    # already_extracted_audios = os.listdir(output_dir)
+    for video_file in tqdm(os.listdir(video_folder_path)):
+    #     if audio_exists(video_file, already_extracted_audios):
+    #         continue
+
+        video_path = os.path.join(video_folder_path, video_file)
         video_id = video_path.split('/')[-1].split('.')[0]
-        #output_path = f'{output_dir}{video_id}.wav'
-        output_path = os.path.join(output_dir, f'{video_id}.wav')
+        output_path = f'{output_dir}{video_id}.wav'
+        
         args = [
             'ffmpeg',
             '-i', video_path,
@@ -31,21 +33,17 @@ def extract_audio(video_folder_path: str, output_dir: str):
         proc.check_returncode()
 
 # Extracts embeddings and saves in output dir
-def extract_embeddings(audio_file_path: str, output_dir: str, device, windows_os = False):
+def extract_embeddings(audio_file_path: str, output_dir: str):
     # track embeddings already extracted
     already_extracted_embeddings = os.listdir(output_dir)
 
-    model = whisper.load_model('base', device=device)
+    model = whisper.load_model('base', device='cuda')
     print('Converting to audio files to embeddings..')
     for audio_file in tqdm(os.listdir(audio_file_path)):
         if embedding_exists(audio_file, already_extracted_embeddings):
             continue
 
-        if (windows_os):
-            audio_path = os.path.join(audio_file_path, audio_file).replace('\\', '/')
-        else:
-            audio_path = os.path.join(audio_file_path, audio_file)
-
+        audio_path = os.path.join(audio_file_path, audio_file)
         id = audio_path.split('/')[-1].split('.')[0]
         output_path = f'{output_dir}{id}.pt'
 
@@ -67,6 +65,13 @@ def extract_embeddings(audio_file_path: str, output_dir: str, device, windows_os
 def embedding_exists(audio_file: str, already_extracted_embeddings: list):
     embedding_file_name = f'{audio_file.split(".")[0]}.pt'
     if embedding_file_name in already_extracted_embeddings:
+        return True
+    else:
+        return False
+    
+def audio_exists(video_file: str, already_extracted_audios: list):
+    audio_file_name = f'{video_file.split(".")[0]}.wav'
+    if audio_file_name in already_extracted_audios:
         return True
     else:
         return False
