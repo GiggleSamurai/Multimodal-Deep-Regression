3ebd8bf09f9c4a8a7df20c77ee46b4ecacb8170e
Louis Wong <gigglesamurai@gmail.com>
2023-07-25 22:48:24 -0700

    Various bug fix on The_Multimodal_Final.ipynb

--- models/Multimodal.py
+++ models/Multimodal.py
@@ -4,13 +4,21 @@ import torch.nn as nn
 # Combine the Multiple modules into a single model
 class EnsembleModel(nn.Module):
 
-    def __init__(self, model1, model2):
+    def __init__(self, model1, model2, deep=False):
         super().__init__()
 
         self.model1 = model1
         self.model2 = model2
-
+        self.deep = deep
         self.regressor = nn.Sequential(
+        nn.Linear(256*2, 256*2),
+        nn.Linear(256*2, 1))
+        
+        self.regressor2 = nn.Sequential(
+        nn.Linear(256*2, 256*2),
+        nn.Linear(256*2, 256*2),
+        nn.Linear(256*2, 256*2),
+        nn.Linear(256*2, 256*2),
         nn.Linear(256*2, 256*2),
         nn.Linear(256*2, 1)) 
 
@@ -18,7 +26,10 @@ class EnsembleModel(nn.Module):
         video_embed = self.model1(video_embed)
         audio_embed = self.model2(audio_embed)
         output = torch.cat((video_embed, audio_embed), dim=1)
-        output = self.regressor(output)
+        if self.deep:
+            output = self.regressor2(output)
+        else:
+            output = self.regressor(output)
         return output
 
 
