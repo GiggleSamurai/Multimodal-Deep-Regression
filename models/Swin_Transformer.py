94f462911f18d85a3e9f6c0f69a2c73e53e90f62
Ahmed <ahmedkasalih@gmail.com>
2023-07-28 17:58:55 -0400

    Swin and Multimodal Classification

--- models/Swin_Transformer.py
+++ models/Swin_Transformer.py
@@ -9,7 +9,8 @@ from util.video_swin_transformer import SwinTransformer3D
 
 
 class Swin_Transformer_model(nn.Module):
-    def __init__(self, linear_in_dim=1, patch_size=(4,4,4), embed_dim=96, drop_rate=0, depths=[2, 2, 6, 2], num_heads=[3, 6, 12, 24], patch_norm=False, window_size=(2,7,7), scaled_target=False):
+    def __init__(self, linear_in_dim=1, patch_size=(4,4,4), embed_dim=96, drop_rate=0, depths=[2, 2, 6, 2], num_heads=[3, 6, 12, 24], patch_norm=False, window_size=(2,7,7), 
+    scaled_target=False, out_features=1, classification_target=False ):
         super(Swin_Transformer_model, self).__init__()
         self.swin_transformer = SwinTransformer3D(
             patch_size=patch_size,
@@ -28,11 +29,12 @@ class Swin_Transformer_model(nn.Module):
             # drop_path_rate=0.4
         )
         self.scaled_target = scaled_target
+        self.classification_target = classification_target
         self.flatten = nn.Flatten()
-        if  not self.scaled_target:
+        if not self.scaled_target and not self.classification_target:
             self.relu = nn.ReLU()
 
-        self.linear1 = nn.Linear(in_features=linear_in_dim, out_features=1)
+        self.linear1 = nn.Linear(in_features=linear_in_dim, out_features=out_features)
         # self.linear2 = nn.Linear(in_features=10, out_features=1)
         # self.fc = nn.Sequential(
         #     nn.Linear(in_features=linear_in_dim, out_features=10),
@@ -49,7 +51,7 @@ class Swin_Transformer_model(nn.Module):
         # print(x.shape)
         x = self.flatten(x)
         # print(x.shape)
-        if  not self.scaled_target:
+        if  not self.scaled_target and not self.classification_target:
             x = self.relu(self.linear1(x))
         else:
             x = self.linear1(x)
